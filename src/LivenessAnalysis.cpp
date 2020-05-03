c1c63197b468cfb85627020cf6bd468a1b34e4d9
jinoopy <jinoopy>
2020-05-03 17:00:01 +0900

    [jinu-dev] LivenessAnalysis SearchAllInst() complete, modified run()

--- src/LivenessAnalysis.cpp
+++ src/LivenessAnalysis.cpp
@@ -4,9 +4,9 @@ RegisterGraph LivenessAnalysis::run(Module &M, ModuleAnalysisManager &MAM) {
     
     vector<Value*> values = SearchAllArgInst(M);
 
-    map<Value*, bool[]> live = LiveInterval(M, values);
+    map<Instruction*, vector<bool>> live = LiveInterval(M, values);
 
-    vector<bool[]> live_values;
+    vector<vector<bool>> live_values;
     for(auto it = live.begin(); it != live.end(); ++it) {
         live_values.push_back(it->second);
     }
@@ -14,4 +14,39 @@ RegisterGraph LivenessAnalysis::run(Module &M, ModuleAnalysisManager &MAM) {
     RegisterGraph graph = RegisterClique(M, live_values);
 
     return graph;
+}
+
+#define isStore(I) (dyn_cast<StoreInst>(&I)==nullptr)
+
+vector<Value*> SearchAllArgInst(Module& M) {
+    vector<Value*> values;
+    for(Function& F : M) {
+        for(Argument& Arg : F.args()) {
+            values.push_back(&Arg);
+        }
+        for(BasicBlock& BB : F) {
+            for(Instruction& I : BB) {
+                if(!I.isTerminator() && !isStore(I)) {
+                    values.push_back(&I);
+                }
+            }
+        }
+    }
+    return values;
+}
+
+map<Instruction*, vector<bool>> LiveInterval(Module& M, vector<Value*>& values) {
+
+    map<Instruction*, vector<bool>> live;
+
+    int N = values.size();
+
+    for(Function& F : M) {
+        for(BasicBlock& BB : F) {
+            for(Instruction& I : BB) {
+                live[&I] = vector<bool>();
+            }
+        }
+    }
+
 }
--- src/LivenessAnalysis.h
+++ src/LivenessAnalysis.h
@@ -20,15 +20,26 @@ class RegisterGraph {
 
 };
 
-class LivenessAnalysis : PassInfoMixin<LivenessAnalysis>{
-
+//Finds all instructions that can be 'alive' in some point.
+//i.e. finds all Arguments & Instructions which refer to 'values'.
+//GVs and non-value Inst.(store, br, ...) are not considered.
 vector<Value*>  SearchAllArgInst(Module&);
-map<Value*, bool[]> LiveInterval(Module&, vector<Value*>&);
-RegisterGraph RegisterClique(Module&, vector<bool[]>&);
+
+//Recursively(post-order) searches through all instructions
+//mark liveness of each values in each instruction
+template<int N>
+map<Instruction*, vector<bool>> LiveInterval(Module&, vector<Value*>&);
+
+//for all bool[] array, if two values are marked 'true' together,
+//there exists a line between two values.
+RegisterGraph RegisterClique(Module&, vector<vector<bool>>&);
+
+class LivenessAnalysis : PassInfoMixin<LivenessAnalysis>{
 
 public:
   RegisterGraph run(Module &M, ModuleAnalysisManager &MAM);
 };
+
 }
 
 extern "C" ::llvm::PassPluginLibraryInfo
