59e69de46c4cf9a63c6b4fb400d5bd12336ecc22
jinoopy <jinoopy>
2020-05-09 22:00:22 +0900

    [jinu-dev] finished template of MallocToGVPass, TODO method implementations

--- src/.vscode/MallocToGVPass.cpp
+++ src/.vscode/MallocToGVPass.cpp
@@ -9,6 +9,34 @@ namespace optim
 
 PreservedAnalyses MallocToGVPass::run(Module &M, ModuleAnalysisManager &MAM)
 {
+    this->M = &M;
+    this->MAM = &MAM;
+
+    Function* FMain = M.getFunction("main");
+    for(Instruction& I : FMain->getEntryBlock()) {
+        CallInst* CallI = dyn_cast<CallInst>(&I);
+        if(CallI != nullptr && CallI->getCalledFunction()->getName() == "malloc") {
+            
+            Type* T = I.getType();
+            assert(T!=nullptr && "return of malloc should be a pointer");
+
+            //TODO
+            Constant* initVal = getInitVal(&I);
+
+            //create corresponding GV
+            GlobalVariable* GV = new GlobalVariable(
+                /*Module=*/ M,
+                /*Type=*/ T,
+                /*isConstant=*/ false,
+                /*Linkage=*/ GlobalValue::InternalLinkage,
+                /*Initializer=*/ initVal,
+                /*Name=*/ (I.hasName()? "gv."+I.getName() : "")
+            );
+            replaceAllUseToGV(&I, GV);
+            //by AliasAnalysis, swap all uses of CallI to
+        }
+    }
+
 }
 
 } // namespace optim
--- src/.vscode/MallocToGVPass.h
+++ src/.vscode/MallocToGVPass.h
@@ -2,36 +2,55 @@
 #define MALLOC_GV_H
 
 #include "llvm/IR/PassManager.h"
+#include "llvm/IR/GlobalVariable.h"
+
 #include "llvm/Passes/PassBuilder.h"
 #include "llvm/Passes/PassPlugin.h"
+
 #include "llvm/Support/raw_ostream.h"
-#include<string>
+#include <string>
+
 using namespace llvm;
 
-namespace {
-class MallocToGVPass : public PassInfoMixin<MallocToGVPass> {
+namespace optim
+{
+class MallocToGVPass : public PassInfoMixin<MallocToGVPass>
+{
 public:
-  PreservedAnalyses run(Module &, ModuleAnalysisManager &);
+    //run() Transforms:
+    //All malloc() in the entry block of main() to GV
+    //all uses of malloc in main to GV
+    PreservedAnalyses run(Module &, ModuleAnalysisManager &);
+
+private:
+    Module* M;
+    ModuleAnalysisManager* MAM;
+
+    Constant* getInitVal(Instruction* ptr);
+
+    void replaceAllUseToGV(Instruction* malloc, GlobalVariable* gv);
+
 };
-}
 
 extern "C" ::llvm::PassPluginLibraryInfo
-llvmGetPassPluginInfo() {
-  return {
-    LLVM_PLUGIN_API_VERSION, "MallocToGVPass", "v0.1",
-    [](PassBuilder &PB) {
-      PB.registerPipelineParsingCallback(
-        [](StringRef Name, ModulePassManager &MPM,
-           ArrayRef<PassBuilder::PipelineElement>) {
-          if (Name == "malloc-gv") {
-            MPM.addPass(MallocToGVPass());
-            return true;
-          }
-          return false;
-        }
-      );
-    }
-  };
+llvmGetPassPluginInfo()
+{
+    return {
+        LLVM_PLUGIN_API_VERSION, "MallocToGVPass", "v0.1",
+        [](PassBuilder &PB) {
+            PB.registerPipelineParsingCallback(
+                [](StringRef Name, ModulePassManager &MPM,
+                   ArrayRef<PassBuilder::PipelineElement>) {
+                    if (Name == "malloc-gv")
+                    {
+                        MPM.addPass(MallocToGVPass());
+                        return true;
+                    }
+                    return false;
+                });
+        }};
 }
+
+} // namespace optim
 
 #endif
