0c86bb706b40b845b94df818a561fa4811fa5f89
dhbryan75 <dhbryan75@gmail.com>
2020-05-12 02:59:43 +0900

    Add PassManager

--- src/LoopOptimPipe1.cpp
+++ src/LoopOptimPipe1.cpp
@@ -5,18 +5,33 @@ using namespace std;
 
 namespace optim {
 
-PreservedAnalyses LoopOptimPipe1::run(Module &M, ModuleAnalysisManager &MAM) {    
-    for(Module::iterator f = M.begin(), fend = M.end(); f != fend; ++f) {
-            LoopInfo &LI = getAnalysis<LoopInfoWrapperPass>().getLoopInfo();
-            for(Loop &L : LI) {
-                //LPPassManager LPM;
-                //createLoopSimplifyCFGPass();
-                //LICMPass::LICMPass();
-                //createLoopRotatePass(maxHeaderSize);
-                //createLoopUnswitchPass(OS, hasBranchDivergence);
-            }
-     }
+PreservedAnalyses LoopOptimPipe1::run(Module &M, ModuleAnalysisManager &MAM) {   
+    PassManager<Function, FunctionAnalysisManager> FPM;
+    FPM.addPass(LoopSimplify());
+
+    PassManager<Loop, LoopAnalysisManager> LPM;
+    LPM.addPass(*createLCSSAPass());
+    LPM.addPass(*createLICMPass());
+    LPM.addPass(*createLoopRotatePass(10));
+    LPM.addPass(*createLoopUnswitchPass(OS, hasBranchDivergence));
+
+    for(Function &F : M) {
+        FunctionAnalysisManager FAM;
+        FPM.run(F, FAM);
+        LoopInfo &LI = getAnalysis<LoopInfoWrapperPass>().getLoopInfo();
+        for(Loop &L : LI) {
+            LoopAnalysisManager LAM;
+            LPM.run(L, LAM);
+        }
+    }
     return PreservedAnalyses::all();
 }
 
 }
+
+
+//LPPassManager LPM;
+//createLoopSimplifyCFGPass();
+//LICMPass::LICMPass();
+//createLoopRotatePass(maxHeaderSize);
+//createLoopUnswitchPass(OS, hasBranchDivergence);
