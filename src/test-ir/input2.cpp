d5d9eadb1629b2eb267a943a8044343e0760957a
dhbryan75 <dhbryan75@gmail.com>
2020-05-14 14:27:04 +0900

    Add opt.sh and fix input2.cpp

--- src/test-ir/input2.cpp
+++ src/test-ir/input2.cpp
@@ -1,18 +1,18 @@
-int foo(int n) {
-    int a = 5;
-    int result = 0;
-    for(int i=0; i<n; i++) {
-        if(foo2(a)) {
-            result += i;
-        }
-    }
-    return result;
-}
-
-bool foo2(int a) {
+bool foo(int a) {
     int sum = 0;
     for(int i=0; i<a; i++) {
         sum += i;
     }
     return (sum>0);
+}
+
+int foo2(int n) {
+    int a = 5;
+    int result = 0;
+    for(int i=0; i<n; i++) {
+        if(foo(a)) {
+            result += i;
+        }
+    }
+    return result;
 }
