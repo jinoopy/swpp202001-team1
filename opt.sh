2374a5d6675dca0db0b39acab7974639f71a2f59
dhbryan75 <dhbryan75@gmail.com>
2020-05-18 19:47:35 +0900

    Modify opt.sh

--- opt.sh
+++ opt.sh
@@ -1,9 +1,9 @@
 clang -o src/LoopOptimPipe1.so src/LoopOptimPipe1.cpp
 
-clang -S -emit-llvm -o src/test-ir/input1.ll src/test-ir/input1.cpp
-clang -S -emit-llvm -o src/test-ir/input2.ll src/test-ir/input2.cpp
-clang -S -emit-llvm -o src/test-ir/input3.ll src/test-ir/input3.cpp
-clang -S -emit-llvm -o src/test-ir/input4.ll src/test-ir/input4.cpp
+#clang -S -emit-llvm -o src/test-ir/input1.ll src/test-ir/input1.cpp
+#clang -S -emit-llvm -o src/test-ir/input2.ll src/test-ir/input2.cpp
+#clang -S -emit-llvm -o src/test-ir/input3.ll src/test-ir/input3.cpp
+#clang -S -emit-llvm -o src/test-ir/input4.ll src/test-ir/input4.cpp
 
 ~/llvmscript/llvm-10.0-releaseassert/bin/opt -S -loop-simplify -lcssa -licm -loop-rotate -loop-unswitch -o src/test-ir/desired1.ll src/test-ir/input1.ll
 ~/llvmscript/llvm-10.0-releaseassert/bin/opt -S -loop-simplify -lcssa -licm -loop-rotate -loop-unswitch -o src/test-ir/desired2.ll src/test-ir/input2.ll
