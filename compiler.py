4d42e8dab0844c9c22f20d9f76ae88c28f8f9c73
jinoopy <jinoopy>
2020-05-25 00:57:46 +0900

    [jinu-dev] OS-compatibility and minor fixes

--- compiler.py
+++ compiler.py
@@ -2,6 +2,7 @@ import json
 import os
 import subprocess
 import re
+import platform
 
 
 def readConfig():
@@ -25,7 +26,7 @@ def updateConfig(config):
     if mode == 1 :
         print("Write the directory of LLVM/bin.")
         print("Current directory : " + config["llvm-bin-dir"])
-        path = input()
+        path = input("> ")
         while os.path.isdir(path) == False:
             if path[0] == '~':
                 path = os.path.expanduser('~') + path[1:]
@@ -44,7 +45,7 @@ def updateConfig(config):
         print("Format : lowers/numbers")
         print("Example: mem2reg, instcombine")
         while True:
-            passName = input()
+            passName = input("> ")
             if not passName.isalnum() or passName in config["opt-pass"].keys():
                 print("Invalid input")
                 continue
@@ -52,7 +53,7 @@ def updateConfig(config):
         
         print("Write the file name(src/*.cpp) of the new pass file.")
         print("Example: src/XXX.cpp -> XXX.cpp")
-        fileName = input()
+        fileName = input("> ")
         regex = re.compile(r"[\d\w_]+\.cpp")
         while True:
             if not re.fullmatch(regex, fileName):
@@ -61,13 +62,19 @@ def updateConfig(config):
             break
 
         outputName = "".join(ch for ch in fileName if ch.isupper() or ch.isdigit())
-        outputName = "lib" + outputName + ".so"
+        outputName = "lib" + outputName
 
         config["opt-pass"][passName] = {"src" : fileName, "lib" : outputName}
         writeConfig(config)
     
 
 def build(config):
+    if(platform.system() == "Darwin"):
+        ISYSROOT = "-isysroot " + subprocessRun("xcrun --show-sdk-path")
+        LIBTYPE = ".dylib"
+    else:
+        ISYSROOT = ""
+        LIBTYPE = ".so"
     LLVMCONFIG = config["llvm-bin-dir"]+"/llvm-config"
     CXXFLAGS = subprocessRun(LLVMCONFIG + " --cxxflags")
     LDFLAGS = subprocessRun(LLVMCONFIG + " --ldflags")
@@ -78,15 +85,18 @@ def build(config):
     CXXFLAGS= CXXFLAGS + "-std=c++17 -I \"" + SRCROOT + "/include\""
     
     for p in config["opt-pass"].keys():
-        print("==========================")
-        print("Compiling " + "./src/"+config["opt-pass"][p]["src"] + " ...")
-        subprocess.run(CXX + " " + CXXFLAGS + " " + LDFLAGS + " " + LIBS + " src/" + config["opt-pass"][p]["src"] + " -o " + "lib/"+config["opt-pass"][p]["lib"] + " -shared -fPIC", shell=True)
+        print("Compiling " + "./src/"+config["opt-pass"][p]["src"] + " ...\n")
+        subprocess.run(CXX + " " + ISYSROOT + " " + CXXFLAGS + " " + LDFLAGS + " " + LIBS + " src/" + config["opt-pass"][p]["src"] + " -o " + "lib/"+config["opt-pass"][p]["lib"] + LIBTYPE + " -shared -fPIC", shell=True)
         print("Complete!")
-    print("==========================")
 
 def opt(config):
+    if(platform.system() == "Darwin"):
+        LIBTYPE = ".dylib"
+    else:
+        LIBTYPE = ".so"
+
     print("Enter the directory for .ll file.")
-    llvmir = input()
+    llvmir = input("> ")
     
     regex = re.compile(r"[\d\w_/.]+\.ll")
     while True:
@@ -101,19 +111,21 @@ def opt(config):
     presets = list(config["preset-passes"].keys())
     for i, pipe in enumerate(presets):
         print(str(i+1) + " : " + pipe)
-        print("  " + config["preset-passes"][pipe])
+        passes = " ".join([x if x[0]=="-" else "-load lib/lib"+x+LIBTYPE for x in config["preset-passes"][presets[i]]])
+        print("  " + passes)
     print("Enter which pipeline you would like to apply")
     print("1 ~ " + str(len(config["preset-passes"].keys())) + ", or enter 0 for custom execution")
-    mode = int(input())
-    passes = ""
+    mode = int(input("> "))
     if mode != 0 :
-        passes = config["preset-passes"][presets[mode-1]]
+        passes = " ".join([x if x[0]=="-" else "-load-pass-plugin=./lib/lib"+x+LIBTYPE for x in config["preset-passes"][presets[mode-1]]])
     else:
         print("Enter custom sequences of passes")
-        print("Example: -mem2reg -simplifycfg -load XXX.so")
-        passes = input()
-    print(config["llvm-bin-dir"]+"/opt -S " + passes + " -o " + llvmir[:-3]+"_out.ll " + llvmir)
+        print("MacOS Example: -mem2reg -simplifycfg -load XXX.dylib")
+        print("Other OS: -mem2reg -simplifycfg -load XXX.so")
+        passes = input("> ")
+    #print(config["llvm-bin-dir"]+"/opt -S " + passes + " -o " + llvmir[:-3]+"_out.ll " + llvmir)
     subprocessRun(config["llvm-bin-dir"]+"/opt -S " + passes + " -o " + llvmir[:-3]+"_out.ll " + llvmir)
+    print("Complete!")
 
 #def backend()
 
@@ -133,8 +145,9 @@ How the compiler.py works
 '''
 while True:
     print(HELP, end = "")
-    mode = int(input("Enter the mode number(1~4) : "))
+    mode = int(input("Enter the mode number(1~4).\n> "))
     config = readConfig()
+    print("==============================")
     if mode == 1:
         updateConfig(config)
     elif mode == 2:
