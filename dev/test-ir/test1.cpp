4e296882cc1c55d5346a0fbabe88fbda2e32a38f
jjwdi0 <jjwdi0@naver.com>
2020-05-30 21:35:44 +0900

    [deokin-dev] Merge LoopOptPipe2 to master branch (2)

--- dev/test-ir/test1.cpp
+++
@@ -1,7 +0,0 @@
-int f(int n) {
-	int res = 0;
-	for(int i=0; i<n; i++) {
-		res += i;
-	}
-	return res;
-}
--- dev/test-ir/test2.cpp
+++
@@ -1,9 +0,0 @@
-int f(int n, int m) {
-	int res = 0;
-	for(int i=0; i<n; i++) {
-		for(int j=0; j<n; j++) {
-			res += i * j;
-		}
-	}
-	return res;
-}
