71f7e35bdc01498cec948396269ee9782c85e5f7
jjwdi0 <jjwdi0@naver.com>
2020-06-06 17:25:45 +0900

    [Deokin-dev] Add comments in StackHeapAccessGroup.cpp

--- dev/StackHeapAccessGroup.cpp
+++ dev/StackHeapAccessGroup.cpp
@@ -6,6 +6,7 @@ using namespace llvm::PatternMatch;
 
 namespace optim {
 
+// This function returns allocated memory area of Value V.
 AllocType getBlockType(const Value *V) {
 	if(auto *CI = dyn_cast<CallInst>(V)) {
 		if(CI->getCalledFunction()->getName() == "malloc") {
@@ -25,15 +26,19 @@ PreservedAnalyses StackHeapAccessGroupPass::run(Function &F, FunctionAnalysisMan
 	if(F.isDeclaration()) {
 		return PreservedAnalyses::all();
 	}
+	// Store last accessed memory area for each basic block.
 	map<BasicBlock *, AllocType> lastAccessType;
 	DominatorTree DT(F);
 
 	for(BasicBlock &BB : F) {
+		// adjacentList: adjacent list to represent graph
+		// indegree: the number of incoming edges for each vertex
+		// instSet: set of instructions in the basic block
 		map<Instruction *, vector<Instruction *>> adjacentList;
 		map<Instruction *, int> indegree;
 		set<Instruction *> instSet;
 
-		// For debugging
+		// For debugging. Store index for each instruction
 		map<Instruction *, int> indexOfInst;
 
 		for(Instruction &I : BB) {
@@ -46,6 +51,7 @@ PreservedAnalyses StackHeapAccessGroupPass::run(Function &F, FunctionAnalysisMan
 		for(Instruction &I1 : BB) {
 			indexOfInst[&I1] = ++index;
 
+			// Terminator instruction (s.t. ret, br) has incoming edge from every instruction.
 			if(I1.isTerminator()) {
 				for(Instruction &I2 : BB) {
 					if(&I1 != &I2) {
@@ -56,11 +62,13 @@ PreservedAnalyses StackHeapAccessGroupPass::run(Function &F, FunctionAnalysisMan
 				break;
 			}
 
+			// If I1 is LoadInst, then it has edges from StoreInsts with same pointer operand.
 			if(auto *LI = dyn_cast<LoadInst>(&I1)) {
 				for(Instruction &I2 : BB) {
 					if(&I1 == &I2) {
 						break;
 					}
+					// Make edge between LoadInst and StoreInst if two have same pointer operand.
 					if(auto *SI = dyn_cast<StoreInst>(&I2)) {
 						if(SI->getPointerOperand() != LI->getPointerOperand()) {
 							continue;
@@ -68,7 +76,7 @@ PreservedAnalyses StackHeapAccessGroupPass::run(Function &F, FunctionAnalysisMan
 						indegree[&I1]++;
 						adjacentList[&I2].push_back(&I1);
 					}
-				}	
+				}
 			} else if(auto *SI = dyn_cast<StoreInst>(&I1)) {
 				for(Instruction &I2 : BB) {
 					if(&I1 == &I2) {
@@ -88,6 +96,8 @@ PreservedAnalyses StackHeapAccessGroupPass::run(Function &F, FunctionAnalysisMan
 			for(unsigned i = 0; i < I1.getNumOperands(); i++) {
 				Value *value = I1.getOperand(i);
 				Instruction *valueToInst = dyn_cast<Instruction>(value);
+				// If below condition is false, it means ith operand is constant or instruction 
+				// from other basic blocks. 
 				if(valueToInst && instSet.find(valueToInst) != instSet.end()) {
 					indegree[&I1]++;
 					adjacentList[valueToInst].push_back(&I1);
@@ -95,8 +105,10 @@ PreservedAnalyses StackHeapAccessGroupPass::run(Function &F, FunctionAnalysisMan
 			}
 		}
 
+		// set of instructions with heap/stack access
 		set<Instruction *> heapAccess, stackAccess;
 
+		// Investigate access memory area, and insert into proper set.
 		for(Instruction &I : BB) {
 			AllocType type;
 			if(auto *SI = dyn_cast<StoreInst>(&I)) {
@@ -114,17 +126,20 @@ PreservedAnalyses StackHeapAccessGroupPass::run(Function &F, FunctionAnalysisMan
 			}
 		}
 
+		// Both size of set are zero -> BB has no memory access
 		if(heapAccess.size() == 0 && stackAccess.size() == 0) {
 			lastAccessType.emplace(&BB, AllocType::UNKNOWN);
 			continue;
 		}
 
+		// Store results from heap/stack first topological sort.
 		vector<Instruction *> heapFirst, stackFirst;
 		int heapFirstCost, stackFirstCost;
 
 		heapFirstCost = getTopologicalSort(adjacentList, indegree, heapAccess, stackAccess, AllocType::HEAP, heapFirst, indexOfInst);
 		stackFirstCost = getTopologicalSort(adjacentList, indegree, heapAccess, stackAccess, AllocType::STACK, stackFirst, indexOfInst);
 
+		// Iterator to traverse predecessors and find last accessed memory area for each basic block
 		pred_iterator PI = pred_begin(&BB), E = pred_end(&BB);
 		bool heapExist = false, stackExist = false;
 
@@ -137,19 +152,24 @@ PreservedAnalyses StackHeapAccessGroupPass::run(Function &F, FunctionAnalysisMan
 			}
 		}
 
+		// Below condition is true when predecessor has only one type of area as last accessed memory area.
 		if(heapExist ^ stackExist) {
+			// If stackExist is true, then heapFirstCost should increase by 1 because heap
+			// access is following after stack access. Same as heapExist and stackFirstCost.
 			heapFirstCost += stackExist;
 			stackFirstCost += heapExist;
 		}
 
 		vector<Instruction *> *finalState;
 
+		// Choose result with lower cost as an optimal order.
 		if(heapFirstCost < stackFirstCost) {
 			finalState = &heapFirst;
 		} else {
 			finalState = &stackFirst;
 		}
 
+		// Find last store/load instruction in the optimal order.
 		AllocType lastAccess;
 		for(auto &I : *finalState) {
 			if(auto *SI = dyn_cast<StoreInst>(I)) {
@@ -160,6 +180,7 @@ PreservedAnalyses StackHeapAccessGroupPass::run(Function &F, FunctionAnalysisMan
 		}
 		lastAccessType.emplace(&BB, lastAccess);
 
+		// Remove all instructions from BB and reconstruct from using IRBuilder.	
 		for(auto &I : *finalState) {
 			I->removeFromParent();
 		}
@@ -173,64 +194,78 @@ PreservedAnalyses StackHeapAccessGroupPass::run(Function &F, FunctionAnalysisMan
 	return PreservedAnalyses::all();
 }
 
+// This function returns result of topological sort, with initial preference to heap/stack access.
 int StackHeapAccessGroupPass::getTopologicalSort(map<Instruction *, vector<Instruction *>> &adjacentList, map<Instruction *, int> indegree, 
 							set<Instruction *> &heapAccess, set<Instruction *> &stackAccess, AllocType firstType,
 							vector<Instruction *> &result, map<Instruction *, int> &indexOfInst) {
-		
-		assert(firstType != AllocType::UNKNOWN && "Invalid function call: AllocType value is AllocType::UNKNOWN");
-		
-		queue<Instruction *> q;
-		set<Instruction *> visited;
+	// AllocType::UNKNOWN cannot be initial preference.
+	assert(firstType != AllocType::UNKNOWN && "Invalid function call: AllocType value is AllocType::UNKNOWN");
 
-		int resetCount = 0;
+	queue<Instruction *> q;
+	set<Instruction *> visited;
 
-		AllocType canTraverse = firstType;
-		unsigned instCount = indegree.size();
+	int resetCount = 0;
 
-		while(result.size() < instCount) {
-			for(auto &inst : indegree) {
-				if(inst.second == 0 && visited.find(inst.first) == visited.end()) {
-					if(auto *SI = dyn_cast<StoreInst>(inst.first)) {
-						if(getBlockType(SI->getPointerOperand()) != canTraverse) {
-							continue;
-						}
-					} else if(auto *LI = dyn_cast<LoadInst>(inst.first)) {
-						if(getBlockType(LI->getPointerOperand()) != canTraverse) {
-							continue;
-						}
+	// canTraverse stores current preference(heap/stack)
+	AllocType canTraverse = firstType;
+	unsigned instCount = indegree.size();
+
+	// Visit all vertices(instructions)
+	while(result.size() < instCount) {
+		for(auto &inst : indegree) {
+			// If indegree is zero and not visited yet
+			if(inst.second == 0 && visited.find(inst.first) == visited.end()) {
+				// Check whether inst.first has same access memory area as canTraverse.
+				if(auto *SI = dyn_cast<StoreInst>(inst.first)) {
+					if(getBlockType(SI->getPointerOperand()) != canTraverse) {
+						continue;
 					}
-					q.push(inst.first);
-				}
-			}
-
-			while(!q.empty()) {
-				Instruction *inst = q.front();
-				q.pop();
-				visited.insert(inst);
-				result.push_back(inst);
-
-				for(auto &there : adjacentList[inst]) {
-					if(!--indegree[there]) {
-						AllocType type = AllocType::UNKNOWN;
-						
-						if(auto *SI = dyn_cast<StoreInst>(there)) {
-							type = getBlockType(SI->getPointerOperand());
-						} else if(auto *LI = dyn_cast<LoadInst>(there)) {
-							type = getBlockType(LI->getPointerOperand());
-						}
-
-						if(type == AllocType::UNKNOWN || type == canTraverse) {
-							q.push(there);
-						}
+				} else if(auto *LI = dyn_cast<LoadInst>(inst.first)) {
+					if(getBlockType(LI->getPointerOperand()) != canTraverse) {
+						continue;
 					}
 				}
+				// Otherwise enqueue it.
+				q.push(inst.first);
 			}
-
-			canTraverse = (canTraverse == AllocType::HEAP ? AllocType::STACK : AllocType::HEAP);
-			resetCount++;
 		}
 
-		return --resetCount;
+		// Topological sort with BFS
+		while(!q.empty()) {
+			Instruction *inst = q.front();
+			q.pop();
+			visited.insert(inst);
+			result.push_back(inst);
+
+			// For every outgoing edge, 
+			for(auto &there : adjacentList[inst]) {
+				// if there is a vertex which has any incoming edge, 
+				if(!--indegree[there]) {
+					AllocType type = AllocType::UNKNOWN;
+
+					if(auto *SI = dyn_cast<StoreInst>(there)) {
+						type = getBlockType(SI->getPointerOperand());
+					} else if(auto *LI = dyn_cast<LoadInst>(there)) {
+						type = getBlockType(LI->getPointerOperand());
+					}
+
+					// if memory access type is unknown or same as canTraverse
+					// then enqueue it.
+					if(type == AllocType::UNKNOWN || type == canTraverse) {
+						q.push(there);
+					}
+				}
+			}
+		}
+
+		// End of topological sort with preference(canTraverse).
+		// Switch canTraverse to stack/heap and increase resetCount by 1.
+		canTraverse = (canTraverse == AllocType::HEAP ? AllocType::STACK : AllocType::HEAP);
+		resetCount++;
 	}
+
+	// Actual resetCount should be decreased by 1.
+	return --resetCount;
+}
 
 }
